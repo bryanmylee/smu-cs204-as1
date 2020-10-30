7412fa8c5021278e1d15b4aa264440d69c260d2d
Bryan Lee <bryanmylee@gmail.com>
2020-10-30 21:16:32 +0800

    Refactor

--- server.py
+++ server.py
@@ -10,16 +10,26 @@ def get_host_port():
     return host, port
 
 
-def accept(sock, mask, clients):
-    conn, addr = sock.accept()
+def create_async_server_socket(host, port):
+    # Create an INET, STREAM (TCP) socket.
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((host, port))
+    server.listen()
+    server.setblocking(False)
+    print(f"Server waiting for Clients on port {port}")
+    return server
+
+
+def accept_fn(server, mask, clients):
+    conn, addr = server.accept()
     host, port = addr
     clients[conn.fileno()] = conn
     print("Accepted connection from", addr)
     conn.setblocking(False)
-    sel.register(conn, selectors.EVENT_READ, read)
+    sel.register(conn, selectors.EVENT_READ, listen_fn)
 
 
-def read(conn, mask, clients):
+def listen_fn(conn, mask, clients):
     data_bytes = conn.recv(1024)
     if data_bytes:
         print("Echoing", data_bytes.decode("utf-8"), "to", conn)
@@ -34,23 +44,20 @@ def read(conn, mask, clients):
 
 
 if __name__ == "__main__":
+    host, port = get_host_port()
+    server = create_async_server_socket(host, port)
+    # Create a selector with socket object key and a function value that
+    # accepts a socket, mutex mask, and a map from socket file number to socket.
     sel = selectors.DefaultSelector()
-
-    address = get_host_port()
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind(address)
-    sock.listen()
-    sock.setblocking(False)
-    sel.register(sock, selectors.EVENT_READ, accept)
-    _, port = address
-    print(f"Server waiting for Clients on port {port}")
+    sel.register(server, selectors.EVENT_READ, accept_fn)
 
     clients = {}
-
     while True:
-        events = sel.select()
-        for key, mask in events:
-            callback = key.data
-            callback(key.fileobj, mask, clients)
+        # Get all socket object keys in the selector.
+        for key, mask in sel.select():
+            # Either the accept_fn or listen_fn.
+            fn = key.data
+            conn = key.fileobj
+            fn(conn, mask, clients)
 
 
